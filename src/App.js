e6cbe7436989ea838dce54ad61bbfab7335f8ec0
unknown <vomanhdat10998@gmail.com>
2020-08-24 16:37:42 +0700

    'master'

--- src/App.js
+++ src/App.js
@@ -6,22 +6,7 @@ import Content from "./BaiTapThucHanhLayout/Content";
 import Footer from "./BaiTapThucHanhLayout/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <div className="row">
-          <div className="col-3">
-            <Sidebar />
-          </div>
-          <div className="col-9">
-            <Content />
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
